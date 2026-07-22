e1f2d201de1dbe1d13ad8a26515c284d0bb95530
Krishnaveni-CE13 <pkrishna2k24@gmail.com>
2026-07-22 23:09:47 +0800

    output.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,21 @@ module "vpc" {
  source  = "terraform-aws-modules/vpc/aws"
 
 
- name = "changechangechange-vpc" # Change this!!!
+ name = "krish-module-vpc" # Change this!!!
  cidr = "10.0.0.0/16"
 
 
  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+ database_subnets                     = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+  create_database_subnet_group         = true   # creates the aws_db_subnet_group
+  create_database_subnet_route_table   = true   # separate route table so DB subnets aren't tied to app private subnets
+  database_subnet_group_name           = "krish-module-db-subnet-group"
 
 
- enable_nat_gateway   = true
- single_nat_gateway   = true
+ enable_nat_gateway   = false
+ #single_nat_gateway   = true
  enable_dns_hostnames = true
 
 
--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "nat_gateway_ids" {
  value = module.vpc.natgw_ids
 }
+
+output "db_subnet_group_id" {
+ value = module.vpc.database_subnet_group
+}
+
+output "private_subnet_ids" {
+ value = module.vpc.private_subnets
+}
